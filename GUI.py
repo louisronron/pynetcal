5b704458893bc139ee4787bf0874a5a2eba677fe
fallen-geko <62592001+fallen-geko@users.noreply.github.com>
2020-06-24 16:59:12 +1000

    Update GUI.py
    Added Subnetting capability and changed output from a Label widget to a Text widget.

--- GUI.py
+++ GUI.py
@@ -3,6 +3,8 @@ from tkinter.ttk import *
 from tkinter import messagebox
 from ipaddress import IPv4Network, IPv4Address, IPv6Network, IPv6Address
 
+import pyperclip
+
 from pynetcal.ipv6pynetcal import PyNIPv6Address, PyNIPv6Network
 from pynetcal.ipv4pynetcal import PyNIPv4Address, PyNIPv4Network
 
@@ -16,15 +18,22 @@ class Application(Frame):
         self.master = master
         self.pack()
         # tkinter variables
+        self.outputVar = StringVar()
         self.conFromVar = StringVar()
         self.conToVar = StringVar()
         self.optionVar = StringVar()
         self.opvals = ["Statistics","Convert"]
         self.fromtoRadios = []
+        self.flsmwidgets = []
+        self.subcomVar = StringVar()
+        self.priorVar = BooleanVar()
+        # fonts and stuff
+        self.fHint = ("","6","italic")
         # make widgets!
         self.createWidgets()
         self.setOutput("")
         self.optionChange(None)
+        #self.switchCom()
 
     def createWidgets(self):
         # Main Frame... and no I don't mean the computer
@@ -38,15 +47,21 @@ class Application(Frame):
         self.tabFrame = Frame(self.main)
         self.tabFrame.pack(side=TOP,anchor='w',fill='x',expand='yes')
         self.inWidgets(self.tabFrame)
+
+        #
+        self.processWidgets(self.main)
         
         # Output frame
         self.outFrame = LabelFrame(self.main, text="Output")
-        self.outFrame.pack(side=TOP,anchor='e',fill='x',expand='yes')
+        self.outFrame.pack(side=TOP,anchor='n',fill='both')
         self.outWidgets(self.outFrame)
         
     def outWidgets(self, master):
-        self.outLabel = Label(master,text="OUTPUT")
-        self.outLabel.pack(side=TOP,fill='x')
+        self.outScroll = Scrollbar(master)
+        self.outScroll.pack(side=RIGHT,fill='y')
+        self.outText = Text(master,yscrollcommand=self.outScroll.set)
+        self.outText.pack(side=LEFT,fill='x')
+        self.outScroll.config(command=self.outText.yview)
         
     def inWidgets(self, master):
         self.tabs = Notebook(master)
@@ -63,10 +78,12 @@ class Application(Frame):
         # Frame for subnetting
         self.snframe = Frame(master)
         self.snframe.pack(side=TOP)
+        self.snWidgets(self.snframe)
         master.add(self.snframe,text="Subnetting")
 
     def setOutput(self,nstr):
-        self.outLabel.config(text=nstr)
+        self.outText.delete("0.0",END)
+        self.outText.insert("0.0",nstr)
         
     def validateIP(self,ip):
         address = ip
@@ -81,7 +98,75 @@ class Application(Frame):
                 return "ipv4"
             except:
                 return False
-    
+            
+    def decideCommand(self):
+        operation = self.tabs.tab("current")["text"].lower()
+        if operation == 'ip':
+            self.calculateIP()
+        elif operation == 'subnetting':
+            self.calculateSubnets()
+            
+    def calculateSubnets(self):
+        subnetLimit = self.limitEntry.get()
+        network = self.netadd.get()
+        hosts = self.hosts.get()
+        subnets = self.snets.get()
+        priorityHosts = self.priorVar.get()
+        scom = self.subcomVar.get()
+        output = ''
+        # Validation
+        if(not validator.ipv4network(network) and not validator.ipv6network(network)):
+            messagebox.showerror("Invalid Network", "IPv4/IPv6 network address supplied is invalid.")
+            return False
+        isIpv4 = validator.ipv4network(network)
+        isIpv6 = validator.ipv6network(network)
+        if scom == 'flsm':
+            try:
+                subnetLimit = int(subnetLimit)
+            except:
+                subnetLimit=None
+            if not subnetLimit:
+                subnetLimit=None
+            if(not validator.integer(subnets)):
+                messagebox.showerror("Invalid Subnet Number", "Number of subnets must be an integer.")
+                return False
+            elif (not validator.integer(hosts)):
+                messagebox.showerror("Invalid Host Number", "Number of hosts must be an integer.")
+                return False
+            subnetList = []
+            try:
+                if isIpv4:
+                    subnetList = PyNIPv4Network(network).subnets_flsm(int(hosts),int(subnets),priorityHosts)
+                    numOfSubnets = PyNIPv4Network(network).num_of_subnets(int(hosts), int(subnets), priorityHosts)
+                    output = helpers.show_ipv4_subnet_table(network, hosts, subnets, subnetList, numOfSubnets, subnetLimit)
+                else:
+                    subnetList = PyNIPv6Network(network).subnets_flsm(int(hosts),int(subnets),priorityHosts)
+                    numOfSubnets = PyNIPv6Network(network).num_of_subnets(int(hosts), int(subnets), priorityHosts)
+                    output = helpers.show_ipv6_subnet_table(network, hosts, subnets, subnetList, numOfSubnets, subnetLimit)
+                self.setOutput(output)
+                return True
+            except TypeError:
+                messagebox.showerror("Invalid Network", "IPv4/IPv6 network address supplied is invalid.")
+                return False
+            except:
+                messagebox.showerror("Error", "Unknown Error Occured")
+                return False
+        elif scom == "vlsm":
+            ss = self.subSize.get()
+            ss = ss.split(',')
+            hosts = list(map(lambda i: int(i), ss))
+            try:
+                if isIpv4:
+                    subnetList = PyNIPv4Network(network).subnets_vlsm(hosts)
+                    output = helpers.show_ipv4_subnet_table(network, hosts, len(hosts), subnetList, len(hosts))
+                else:
+                    subnetList = PyNIPv6Network(network).subnets_vlsm(hosts)
+                    output = helpers.show_ipv6_subnet_table(network, hosts, len(hosts), subnetList, len(hosts))
+                self.setOutput(output)
+                return True
+            except ValueError:
+                messagebox.showerror("Error", "Specified number of hosts or subnets cannot be accommodated")
+                
     def calculateIP(self):
         ipadd = self.ipEntry.get()
         ipin = self.validateIP(ipadd)
@@ -151,7 +236,7 @@ class Application(Frame):
         self.ipEntryFrame.pack(side=LEFT)
         self.ipEntryLabel = Label(self.ipEntryFrame,text="IP Address")
         self.ipEntryLabel.pack(side=TOP, anchor='w')
-        self.ipEntry = Entry(self.ipEntryFrame)
+        self.ipEntry = Entry(self.ipEntryFrame,width=60)
         self.ipEntry.pack(side=TOP,anchor='w')
         # Options
         self.ipOptionsFrame = Frame(self.ipMain)
@@ -166,9 +251,19 @@ class Application(Frame):
         self.conFrame = Frame(master)
         self.conFrame.pack(side=TOP,anchor='w',fill='x')
         self.conWidgets(self.conFrame)
+
+    def processWidgets(self, master):
+        self.buttonsFrame = Frame(master)
+        self.buttonsFrame.pack(side=TOP)
         # Go!
-        self.ipCalculate = Button(master,text="Calculate",command=self.calculateIP)
-        self.ipCalculate.pack(side=TOP)
+        self.ipCalculate = Button(self.buttonsFrame,text="Calculate",command=self.decideCommand)
+        self.ipCalculate.pack(side=LEFT)
+        # Copy to clipboard
+        self.ipCopyToClip = Button(self.buttonsFrame,text="Copy Output",command=self.copyOutput)
+        self.ipCopyToClip.pack(side=RIGHT)
+
+    def copyOutput(self):
+        pyperclip.copy(self.outText.get("0.0",END))
         
     def conWidgets(self, master):
         self.conLabel = Label(master, text="Conversion")
@@ -199,9 +294,86 @@ class Application(Frame):
         self.toBin.pack(side=TOP,anchor='w')
         self.fromtoRadios.append(self.toBin)
         self.toBin.invoke()
-    
+        
+    def switchCom(self):
+        com = self.subcomVar.get()
+        if com == "vlsm":
+            self.subSize.config(state="normal")
+            for widget in self.flsmwidgets:
+                widget.config(state="disabled")
+        else:
+            self.subSize.config(state="disabled")
+            for widget in self.flsmwidgets:
+                widget.config(state="normal")
+            
+    def snWidgets(self, master):
+        self.netaddLabel = Label(master, text="Network Address")
+        self.netaddLabel.pack(side=TOP,anchor='w')
+        self.netandrad = Frame(master)
+        self.netandrad.pack(side=TOP,anchor='w')
+
+        self.exFrame = Frame(master)
+        self.exFrame.pack(side=TOP,anchor='w')
+        self.hostsFrame = Frame(self.exFrame)
+        self.hostsFrame.pack(side=LEFT,anchor='n')
+        self.hostsLabel = Label(self.hostsFrame,text="Hosts")
+        self.hostsLabel.pack(side=TOP,anchor='w')
+        self.hosts=Entry(self.hostsFrame)
+        self.hosts.pack(side=TOP,anchor='w')
+        self.flsmwidgets.append(self.hosts)
+        
+        self.snetsFrame = Frame(self.exFrame)
+        self.snetsFrame.pack(side=LEFT,anchor='n')
+        self.snetsLabel = Label(self.snetsFrame,text="Subnets")
+        self.snetsLabel.pack(side=TOP,anchor='w')
+        self.snets=Entry(self.snetsFrame)
+        self.snets.pack(side=TOP,anchor='w')
+        self.flsmwidgets.append(self.snets)
+        
+        self.subSizeFrame = Frame(self.exFrame)
+        self.subSizeFrame.pack(side=LEFT,anchor='n')
+        self.subSizeLabel = Label(self.subSizeFrame,text="Subnet Size")
+        self.subSizeLabel.pack(side=TOP,anchor='w')
+        self.subSize = Entry(self.subSizeFrame,width=50)
+        self.subSize.pack(side=TOP, anchor='w')
+        self.subHint = Label(self.subSizeFrame,text="Separate with comma's (\",\")",font=self.fHint)
+        self.subHint.pack(side=TOP, anchor='w')
+        
+        self.moreOpsFrame = Frame(master)
+        self.moreOpsFrame.pack(side=TOP,anchor='w')
+        self.prioFrame = LabelFrame(self.moreOpsFrame, text="Priority")
+        self.prioFrame.pack(side=LEFT)
+        self.limitFrame = LabelFrame(self.moreOpsFrame, text="Limit")
+        self.limitFrame.pack(side=LEFT)
+        
+        # Okay the entry and radios
+        self.netadd = Entry(self.netandrad,width=60)
+        self.netadd.pack(side=LEFT)
+        self.rbFlsm = Radiobutton(self.netandrad, text="flsm", variable=self.subcomVar, value="flsm",command=self.switchCom)
+        self.rbFlsm.pack(side=LEFT)
+        self.rbFlsm.invoke()
+        self.rbVlsm = Radiobutton(self.netandrad, text="vlsm", variable=self.subcomVar, value="vlsm",command=self.switchCom)
+        self.rbVlsm.pack(side=LEFT)
+
+        # Widgets for more opts
+        self.priHosts = Radiobutton(self.prioFrame, text="Hosts", variable=self.priorVar, value=True)
+        self.priHosts.pack(side=TOP,anchor='w')
+        self.priHosts.invoke()
+        self.flsmwidgets.append(self.priHosts)
+        self.priSNets = Radiobutton(self.prioFrame, text="Subnets", variable=self.priorVar, value=False)
+        self.priSNets.pack(side=TOP,anchor='w')
+        self.flsmwidgets.append(self.priSNets)
+
+        self.limitEntry = Entry(self.limitFrame)
+        self.limitEntry.pack(side=TOP)
+        self.flsmwidgets.append(self.limitEntry)
+        self.limitLabel = Label(self.limitFrame,font=self.fHint,text="Use 0 for no limit")
+        self.limitLabel.pack(side=TOP,anchor='w')
+        
+                         
 root = Tk()
 root.title("PyNetCal")
 root.minsize(width=800, height=600)
+root.maxsize(width=800, height=600)
 app = Application(master=root)
 app.mainloop()
